aba035574ba7f4ad92fbdb063c2a9bffeb9b06e9
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-04 22:32:26 +0330

    feat: add navbar and sidebar compnents

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Navbar } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, Sidebar, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
